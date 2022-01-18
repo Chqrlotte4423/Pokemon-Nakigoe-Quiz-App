0c2f9136aa548154895c924c43a4393cf9d1aa59
Saurians <tota-pulchra5810@hotmail.co.jp>
2022-01-18 17:06:13 +0900

    add checked file

---
+++ H-Test_PNapp/H-Test_PNapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H-Test_PNapp
+//
+//  Created by Feoca on 2022/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ H-Test_PNapp/H-Test_PNapp/ZukanViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ZukanViewController.swift
+//  H-Test_PNapp
+//
+//  Created by Feoca on 2022/01/18.
+//
+
+import UIKit
+
+class ZukanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
